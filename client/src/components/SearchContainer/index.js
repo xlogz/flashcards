74a4e5cd8e28afdc3099cd13a43432146cf173df
xlogz <loganxlow@gmail.com>
2020-04-04 14:09:45 -0700

    Added basic search page structure

--- client/src/components/SearchContainer/index.js
+++ client/src/components/SearchContainer/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './styles.css'
 
 function SearchContainer(){
-	return(<div className="search-container">Search Container</div>)
+	return(
+		<div className="search-container">
+		<div className="container">
+			<div className="search-content">
+				<label>Search: &nbsp;
+				<input type="text" placeholder="Title, Keyword, or Tag"></input>
+				</label>
+				or
+
+				<button>Create New Flashcard Set</button>
+			</div>
+			
+		</div></div>
+
+		)
 }
 
 export default SearchContainer;
