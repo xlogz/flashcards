ff329752f08c7df2da6c0d1c0b4d26056f2822e4
xlogz <loganxlow@gmail.com>
2020-04-07 15:41:16 -0700

    Added new card set button and styling. Search integration as well

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,10 +3,29 @@ import './styles.css';
 import { Link } from "react-router-dom";
 import PostAddIcon from '@material-ui/icons/PostAdd';
 
-export default function NavBar() {
-	return (
 
-		<div className="navbar">
+
+export default class NavBar extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleClick(){
+       let searchText = document.getElementsByClassName("navbar-search-text")[0];
+       let searchUnderline = document.getElementsByClassName("navbar-search-underline")[0];
+       console.log(searchUnderline)
+       searchUnderline.style.opacity = 1;
+       searchUnderline.style.width = "200px";
+    }
+
+    render(){
+        return (
+
+        <div className="navbar">
             <div className="navbar-content">
             <div className="logo menu-item">
                 Quizme
@@ -17,28 +36,34 @@ export default function NavBar() {
             <div className="menu-item">
                 <Link to="/flashcards">Flashcards</Link>
             </div>
-            <div className="menu-item">
-                <Link to="/search">Search</Link>
+            <div className="menu-item search" onClick={this.handleClick.bind(this)}>
+
+                <div className="navbar-search-text">Search</div>
+
+                <div className="navbar-search-underline">&nbsp;</div>
             </div>
             
 
             <div className="right-side">
-                <div className="navbar-new-set"><PostAddIcon/></div>
+                <div className="navbar-new-set"><Link to="/home/newcard"><PostAddIcon/></Link></div>
                 <div className="login-link menu-item">
                     <Link to="/login">Login</Link>
                 </div>
                 <div className="sign-up-btn menu-item">
 
-                    Sign Up
+                    <Link to="/signup">Sign Up</Link>
+
                 </div>
 
             </div>
             </div>
 
-		</div>
-			
+        </div>
+            
 
 
 
-		)
+        )
+    }
+	
 }
