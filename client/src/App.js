500d01e3e02cd14261244660bef48b195de1f026
xlogz <loganxlow@gmail.com>
2020-04-10 00:38:08 -0700

    Added login state

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,28 @@ import {
 } from "react-router-dom";
 
 
-export default function SimpleContainer() {
-  return (
+export default class MainApp extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleLogin = this.handleLogin.bind(this);
+		this.state = {loggedIn : false
+		}
+	}
+
+	handleLogin(){
+		this.setState({loggedIn : true});
+	}
+
+	render(){
+		return (
     
-    <React.Fragment>
-    <Router>
-    <NavBar/>
-    
-    	<ContentContainer/>
-    </Router>
-    </React.Fragment>
+	    <React.Fragment>
+	    <Router>
+	    <NavBar loggedIn={this.state.loggedIn}/>
+	    <ContentContainer auth={this.handleLogin} />
+	    </Router>
+	    </React.Fragment>
   );
+	}
+  
 }
